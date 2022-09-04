cd5d282b09bcb77fec85abd06090750017f6fdf3
kxx <132kxx@gmail.com>
2022-09-04 19:28:14 +0900

    #1

---
+++ NavigationViewController_tutorial/NavigationViewController_tutorial/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NavigationViewController_tutorial
+//
+//  Created by kxx: on 2022/09/04.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
